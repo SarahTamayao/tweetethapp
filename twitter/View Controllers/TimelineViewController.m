fa274213c1a3f244210fda3d023826e7de3c7012
Laura Yao <ljyao@fb.com>
2021-06-28 19:02:51 -0400

    timeline

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -10,8 +10,12 @@
 #import "APIManager.h"
 #import "AppDelegate.h"
 #import "LoginViewController.h"
+#import "TweetCell.h"
 
-@interface TimelineViewController ()
+@interface TimelineViewController () <UITableViewDataSource, UITableViewDelegate>
+@property NSMutableArray *arrayOfTweets;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
 
 @end
 
@@ -19,19 +23,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
     
+    [self fetchTweets];
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchTweets) forControlEvents:UIControlEventValueChanged];
+        [self.tableView insertSubview: self.refreshControl atIndex:0];
+    
+    
+   
+}
+
+- (void) fetchTweets {
     // Get timeline
     [[APIManager shared] getHomeTimelineWithCompletion:^(NSArray *tweets, NSError *error) {
         if (tweets) {
             NSLog(@"😎😎😎 Successfully loaded home timeline");
-            for (NSDictionary *dictionary in tweets) {
+            /*for (NSDictionary *dictionary in tweets) {
                 NSString *text = dictionary[@"text"];
                 NSLog(@"%@", text);
-            }
+            }*/
+            self.arrayOfTweets =(NSMutableArray *) tweets;
+            [self.tableView reloadData];
+
         } else {
             NSLog(@"😫😫😫 Error getting home timeline: %@", error.localizedDescription);
         }
     }];
+    [self.refreshControl endRefreshing];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -47,6 +67,34 @@
     appDelegate.window.rootViewController = loginViewController;
     [[APIManager shared] logout];
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.arrayOfTweets.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    TweetCell *cell = (TweetCell *) [tableView dequeueReusableCellWithIdentifier:@"TweetCell" forIndexPath:indexPath];
+    cell.tweet = self.arrayOfTweets[indexPath.row];
+    cell.nameOfUser.text = cell.tweet.user.name;
+    cell.userNameT.text = cell.tweet.user.screenName;
+    cell.tweetText.text = cell.tweet.text;
+    cell.tweetDate.text = cell.tweet.createdAtString;
+    NSString* favCount = [NSString stringWithFormat:@"%i", cell.tweet.favoriteCount];
+    NSString* replyCount =[NSString stringWithFormat:@"%i", cell.tweet.retweetCount];
+    
+    [cell.likeB setTitle:favCount forState:UIControlStateNormal];
+    [cell.retweetB setTitle:replyCount forState:UIControlStateNormal];
+    
+    NSString *URLString = cell.tweet.user.profilePicture;
+    NSURL *url = [NSURL URLWithString:URLString];
+    NSData *urlData = [NSData dataWithContentsOfURL:url];
+    cell.profilePhoto.image = [UIImage imageWithData:urlData];
+    
+    
+    
+    return cell;
+}
+
 
 /*
 #pragma mark - Navigation
---
+++ twitter/Views/TweetCell.h
@@ -0,0 +1,28 @@
+//
+//  TweetCell.h
+//  twitter
+//
+//  Created by Laura Yao on 6/28/21.
+//  Copyright © 2021 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Tweet.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TweetCell : UITableViewCell
+@property (strong, nonatomic) Tweet *tweet;
+@property (weak, nonatomic) IBOutlet UIImageView *profilePhoto;
+@property (weak, nonatomic) IBOutlet UILabel *nameOfUser;
+@property (weak, nonatomic) IBOutlet UILabel *userNameT;
+@property (weak, nonatomic) IBOutlet UILabel *tweetDate;
+@property (weak, nonatomic) IBOutlet UILabel *tweetText;
+@property (weak, nonatomic) IBOutlet UIButton *replyB;
+@property (weak, nonatomic) IBOutlet UIButton *retweetB;
+@property (weak, nonatomic) IBOutlet UIButton *likeB;
+@property (weak, nonatomic) IBOutlet UIButton *messageB;
+
+@end
+
+NS_ASSUME_NONNULL_END
